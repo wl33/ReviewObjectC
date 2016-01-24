bfdf11284fb91b5a52c231b3d5e2e116e539a1c0
wl33 <linewishuo@weishuos-mbp>
2016-01-24 20:59:55 +0000

    Initial Commit

---
+++ ReviewObjectC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ReviewObjectC
+//
+//  Created by WEISHUO LIN on 24/01/2016.
+//  Copyright © 2016 WEISHUO LIN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
