c5f3737c4dc4edcd10f73270f5c9247f9c4b93c8
wl33 <linewishuo@weishuos-mbp.lan>
2016-01-24 21:41:07 +0000

    test struct

--- ReviewObjectC/main.m
+++ ReviewObjectC/main.m
@@ -8,10 +8,23 @@
 
 #import <Foundation/Foundation.h>
 
+typedef struct{
+    char color[10];
+    char branch[10];
+}Car;
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        Car car= {"red","BMW"};
+//        car->branch = "BMW";
+//        car.color = "red";
+//        
+        NSLog(@"This car is in %s and is from %s",car.branch,car.color);
+        
+        
     }
     return 0;
 }
